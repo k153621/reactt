74dfffc44ea37c632b6758541562538c8c046f2b
Hira Fatima <hirafatima043@gmail.com>
2020-06-03 19:33:37 +0500

    Updates

---
+++ precache-manifest.2ef9ba31d226d8a9e0b198345494edf6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f83b33ae38a9eb3de340cd67659f501e",
+    "url": "/reactt/index.html"
+  },
+  {
+    "revision": "42654bd4016de502fc48",
+    "url": "/reactt/static/css/2.fba5cc5c.chunk.css"
+  },
+  {
+    "revision": "dc786120e20c708e1f2e",
+    "url": "/reactt/static/css/main.8f09bc47.chunk.css"
+  },
+  {
+    "revision": "42654bd4016de502fc48",
+    "url": "/reactt/static/js/2.1ad5ebbd.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/reactt/static/js/2.1ad5ebbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc786120e20c708e1f2e",
+    "url": "/reactt/static/js/main.c0cfdd74.chunk.js"
+  },
+  {
+    "revision": "f1affba14b89b9de8b1f",
+    "url": "/reactt/static/js/runtime-main.396b6b47.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reactt/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reactt/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reactt/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactt/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reactt/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactt/static/media/logo.5d5d9eef.svg"
+  }
+]);
